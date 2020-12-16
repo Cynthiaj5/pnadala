940a1fc566c1c12bacd46d3b353ca9ca2357c701
Cynthiaj5 <73096027+Cynthiaj5@users.noreply.github.com>
2020-12-16 09:14:28 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
+  
 function setup() {
   createCanvas(400, 400);
+  noStroke();
 }
 
 function draw() {
-    var amplee= random(10,40);
+  var amplee= random(10,40);
   background(255,5);
-  fill(255,0,0);
-  ellipse(random(400)), random(400), amplee,amplee):
+   fill(255,0,0);
+   ellipse(random(400), random(400), amplee,amplee);
 }
